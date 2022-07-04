d844e3d7b3d8df291c4b8925477cfc79094c909d
Aman <amandeep1994@msn.com>
2022-07-04 16:30:34 +0100

    Intial Project Configuiration

---
+++ README.md
@@ -0,0 +1,1 @@
+# HealthX - Client Service Implementation
---
+++ src/test/java/com/abidevel/oauth/healthxclient/HealthxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abidevel.oauth.healthxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthxClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
